395d7d202266cc9385f0df9ed603f87b0dce235a
unknown <pamelanmuller@gmail.com>
2025-11-05 22:20:32 -0300

    Incluido teste automatizado para o professor incluir atividade nas aulas e realizado um ajuste no teste de criar aula

--- test/professorAdicionaAtividadeNaAula.test.js
+++ test/professorAdicionaAtividadeNaAula.test.js
@@ -4,75 +4,71 @@ require('dotenv').config()
 const { obtertokenProfessor } = require('../helpers/autenticacaoProfessor')
 const { obtertokenAluno } = require('../helpers/autenticacaoAluno')
 
-
-describe('Professor cria a aula', () => {
+describe('Professor adiciona atividade nas aulas', () => {
     let tokenProfessor
 
         beforeEach(async () => {
             tokenProfessor = await obtertokenProfessor('gil@teste.com','123456','professor')
         })
 
-    describe('POST /aulas', () => {
-        it('Deve retornar sucesso com 201 Aula criada', async () => {
+    describe('POST /atividades', () => {
+        it('Deve retornar sucesso com 201 Atividade criada', async () => {
+            const aulaId = 1
+            
             const resposta = await request(process.env.BASE_URL)
-                .post('/api/aulas')
+                .post(`/api/aulas/${aulaId}/atividades`)
                 .set('Content-Type','application/json')
                 .set('Authorization','Bearer '+ tokenProfessor)
                 .send({
-                       "titulo": "Aula teste automatizado",
-                       "descricao": "Aula criada durante o teste automatizado",
-                       "alunoId": 1
+                       "descricao": "Atividade criada durante o teste automatizado",
                       })
 
                 expect(resposta.status).to.equal(201);
-        })       
-    })
-})
+        })
 
-    
-describe('Professor tenta criar a aula sem estar logado', () => {
-
-    describe('POST /aulas', () => {
-        it('Deve retornar 401 Não autenticado', async () => {
+        it('Deve retornar 404 Aula não encontrada', async () => {
+            const aulaId = 9999
+            
             const resposta = await request(process.env.BASE_URL)
-                .post('/api/aulas')
+                .post(`/api/aulas/${aulaId}/atividades`)
                 .set('Content-Type','application/json')
+                .set('Authorization','Bearer '+ tokenProfessor)
                 .send({
-                       "titulo": "Aula teste automatizado sem o token",
-                       "descricao": "Aula criada durante o teste automatizado sem o token",
-                       "alunoId": 1
+                       "descricao": "Atividade criada durante o teste automatizado porém não deve ser encontrada",
                       })
 
-                expect(resposta.status).to.equal(401);
+                expect(resposta.status).to.equal(404);
         })
 
        
     })
+
+    
+
 })
 
-describe('Aluno tenta criar aula', () => {
+describe('Aluno tenta adicionar atividade nas aulas', () => {
     let tokenAluno
 
         beforeEach(async () => {
             tokenAluno = await obtertokenAluno('pamela.persuhn@teste.com','123456','aluno')
         })
 
-    describe('POST /aulas', () => {
+    describe('POST /atividades', () => {
         it('Deve retornar 403 Acesso negado', async () => {
+            const aulaId = 1
+            
             const resposta = await request(process.env.BASE_URL)
-                .post('/api/aulas')
+                .post(`/api/aulas/${aulaId}/atividades`)
                 .set('Content-Type','application/json')
                 .set('Authorization','Bearer '+ tokenAluno)
                 .send({
-                       "titulo": "Aula teste automatizado criada pelo aluno",
-                       "descricao": "Aula criada durante o teste automatizado pelo aluno",
-                       "alunoId": 1
+                       "descricao": "Atividade criada durante o teste automatizado por um aluno",
                       })
 
                 expect(resposta.status).to.equal(403);
         })
-       
+     
     })
 
 })
-
